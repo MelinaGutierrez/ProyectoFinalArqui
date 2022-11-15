82165ae9ce5ad135a1a3b9835307700f96a14885
Melina <melina.gutierrez@ucb.edu.bo>
2022-11-15 17:53:55 -0400

    Agregar archivos de proyecto.

---
+++ ProyectoFinalArqui/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ProyectoFinalArqui
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public static string Something()
+        {
+            return "Algo";
+        }
+
+        public static bool Login(string user, string pass) =>
+            user == "marco" && pass == "12345" ? true : false;
+    }
+}
---
+++ TestProyectoFinal/UnitTest1.cs
@@ -0,0 +1,48 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestProyectoFinal
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string result = ProyectoFinalArqui.Program.Something();
+            Assert.AreEqual("Algo", result);
+        }
+
+        [TestMethod]
+        public void TestPlanCorrecto()
+        {
+            bool result = ProyectoFinalArqui.Program.cobroAddons("Plan S a medida", "DATOS XS");
+            Assert.AreEqual(10, result);
+        }
+
+        public void TestPlanPrepago()
+        {
+            bool result = ProyectoFinalArqui.Program.tipoPlan("PREPAGO LIBRE");
+            Assert.AreEqual(1.70, result);
+        }
+
+        [TestMethod]
+        public void TestVigencia()
+        {
+            bool result = ProyectoFinalArqui.Program.validarVigencia("1-05-2022", "30-05-2022");
+            Assert.AreEqual(29, result);
+        }
+
+        public void TestMonto()
+        {
+            bool result = ProyectoFinalArqui.Program.totalCosto("Plan S a medida", "DATOS M", "Youtube");
+            Assert.AreEqual(182, result);
+        }
+
+        public void TestAcceso()
+        {
+            bool result = ProyectoFinalArqui.Program.accesoAddon("Plan S a medida");
+            Assert.AreEqual(true, result);
+        }
+
+    }
+}
