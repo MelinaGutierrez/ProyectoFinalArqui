46befb69821faeb3c0f465a0063c85d01779e2f2
Marco <marco.escobar@ucb.edu.bo>
2022-11-18 16:00:50 -0400

    Pruebas Validar Addons

--- ProyectoFinalArqui/Program.cs
+++ ProyectoFinalArqui/Program.cs
@@ -8,19 +8,10 @@ namespace ProyectoFinalArqui
         {
             Console.WriteLine("Hello World!");
         }
-
-        public static string Something()
-        {
-            return "Algo";
-        }
-
-        public static bool Login(string user, string pass) =>
-            user == "marco" && pass == "12345" ? true : false;
-
-        public static double tipoPlan(string prepagoLibre)
+        public static double tipoPlan(string tipoPlan)
         {
             double precio = 0;
-            switch (prepagoLibre)
+            switch (tipoPlan)
             {
                 case "PREPAGO LIBRE":
                     precio = 1.70; 
@@ -39,9 +30,114 @@ namespace ProyectoFinalArqui
                     break;
                 case "Plan L a medida":
                     precio = 150; 
-                    break; 
+                    break;
             }
             return precio;
+        }
+
+        public static double cobroAddons(string plan, string tipoAddon)
+        {
+            double precio = 0; 
+            if (plan == "Plan S a medida" || plan == "Plan M a medida" || plan == "Plan L a medida")
+            {
+                switch (tipoAddon)
+                {
+                    case "DATOS XS":
+                        precio = 10; 
+                        break;
+                    case "DATOS S":
+                        precio = 33; 
+                        break;
+                    case "DATOS M":
+                        precio = 82; 
+                        break;
+                    case "DATOS L":
+                        precio = 112; 
+                        break;
+                    case "DATOS XL":
+                        precio = 212; 
+                        break;
+                    case "DATOS XXL":
+                        precio = 353; 
+                        break;
+                }
+            }
+            return precio;
+        }
+        public static double cantidadAddons(string plan, string tipoAddon)
+        {
+            double cantidad = 0; 
+            if (plan == "Plan S a medida" || plan == "Plan M a medida" || plan == "Plan L a medida")
+            {
+                switch (tipoAddon)
+                {
+                    case "DATOS XS":
+                        cantidad = 400; 
+                        break;
+                    case "DATOS S":
+                        cantidad = 1200; 
+                        break;
+                    case "DATOS M":
+                        cantidad = 4200; 
+                        break;
+                    case "DATOS L":
+                        cantidad = 7200; 
+                        break;
+                    case "DATOS XL":
+                        cantidad = 13700; 
+                        break;
+                    case "DATOS XXL":
+                        cantidad = 23200; 
+                        break;
+                }
+            }
+            return cantidad;
+        }
+
+        public static double addonCredito(string datoAddon, string planAddon)
+        {
+            double precio = 0; 
+            if (datoAddon == "Addon on")
+            {
+                switch (planAddon)
+                {
+                    case "M":
+                        precio = 20; 
+                        break;
+                    case "L":
+                        precio = 50; 
+                        break;
+                    case "XL":
+                        precio = 80; 
+                        break;
+                    case "Youtube":
+                        precio = 50;
+                        break;
+                    case "Vimeo":
+                        precio = 50;
+                        break;
+                }
+            }
+            return precio;
+        }
+
+        public static double totalCosto(string plan, string addon, string adicional)
+        {
+            double valor1 = tipoPlan(plan);
+            double valor2 = cobroAddons(plan,addon);
+            double valor3 = addonCredito("Addon on", adicional);
+            return valor1 + valor2 + valor3;
+        }
+
+        public static bool accesoAddon(string plan)
+        {
+            bool value = false;
+            if (plan == "Plan S a medida" || plan == "Plan M a medida" || plan == "Plan L a medida"
+                || plan == "PLAN WAO")
+            {
+                value = true;
+            }
+            return value;
         }
     }
 }
--- TestProyectoFinal/UnitTest1.cs
+++ TestProyectoFinal/UnitTest1.cs
@@ -6,45 +6,74 @@ namespace TestProyectoFinal
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
-        {
-            string result = ProyectoFinalArqui.Program.Something();
-            Assert.AreEqual("Algo", result);
-        }
-
-        [TestMethod]
-        public void TestPlanPrepago()
+        public void TestMontoPlanes()
         {
             double result = ProyectoFinalArqui.Program.tipoPlan("PREPAGO LIBRE");
             Assert.AreEqual(1.70, result);
+            double result2 = ProyectoFinalArqui.Program.tipoPlan("Plan S a medida");
+            Assert.AreEqual(50, result2);
+            double result3 = ProyectoFinalArqui.Program.tipoPlan("PLAN WAO");
+            Assert.AreEqual(200, result3);
         }
-        /*
+        
         [TestMethod] 
-        public void TestPlanCorrecto()
+        public void TestCostoCorrectoAddons()
         {
-            bool result = ProyectoFinalArqui.Program.cobroAddons("Plan S a medida", "DATOS XS");
+            double result = ProyectoFinalArqui.Program.cobroAddons("Plan S a medida", "DATOS XS");
             Assert.AreEqual(10, result);
+            double result2 = ProyectoFinalArqui.Program.cobroAddons("PREPAGO LIBRE", "DATOS XS");
+            Assert.AreEqual(0, result2);
+            double result3 = ProyectoFinalArqui.Program.cobroAddons("Plan L a medida", "DATOS S");
+            Assert.AreEqual(33, result3);
+            double result4 = ProyectoFinalArqui.Program.cobroAddons("Plan S a medida", "DATOS XXL");
+            Assert.AreEqual(353, result4);
         }
-
-
-        [TestMethod]
-        public void TestVigencia()
+        [TestMethod] 
+        public void TestMontoCorrectoAddons()
         {
-            bool result = ProyectoFinalArqui.Program.validarVigencia("1-05-2022", "30-05-2022");
-            Assert.AreEqual(29, result);
+            double result = ProyectoFinalArqui.Program.cantidadAddons("Plan S a medida", "DATOS XS");
+            Assert.AreEqual(400, result);
+            double result2 = ProyectoFinalArqui.Program.cantidadAddons("PREPAGO LIBRE", "DATOS XS");
+            Assert.AreEqual(0, result2);
+            double result3 = ProyectoFinalArqui.Program.cantidadAddons("Plan L a medida", "DATOS S");
+            Assert.AreEqual(1200, result3);
+            double result4 = ProyectoFinalArqui.Program.cantidadAddons("Plan S a medida", "DATOS XXL");
+            Assert.AreEqual(23200, result4);
         }
-
+        [TestMethod] 
+        public void TestAddonCredito()
+        {
+            double result = ProyectoFinalArqui.Program.addonCredito("Addon on", "M");
+            Assert.AreEqual(20, result);
+            double result2 = ProyectoFinalArqui.Program.addonCredito("Addon on", "L");
+            Assert.AreEqual(50, result2);
+            double result3 = ProyectoFinalArqui.Program.addonCredito("Addon on", "XL");
+            Assert.AreEqual(80, result3);
+            double result4 = ProyectoFinalArqui.Program.addonCredito("Addon off", "XL");
+            Assert.AreEqual(0, result4);
+        }
+        
+        // [TestMethod]
+        // public void TestVigencia()
+        // {
+        //     bool result = ProyectoFinalArqui.Program.validarVigencia("1-05-2022", "30-05-2022");
+        //     Assert.AreEqual(29, result);
+        // }
+        [TestMethod]
         public void TestMonto()
         {
-            bool result = ProyectoFinalArqui.Program.totalCosto("Plan S a medida", "DATOS M", "Youtube");
+            double result = ProyectoFinalArqui.Program.totalCosto("Plan S a medida", "DATOS M", "Youtube");
             Assert.AreEqual(182, result);
         }
-
+        [TestMethod] 
         public void TestAcceso()
         {
             bool result = ProyectoFinalArqui.Program.accesoAddon("Plan S a medida");
             Assert.AreEqual(true, result);
+            bool result2 = ProyectoFinalArqui.Program.accesoAddon("PLAN WAO");
+            Assert.AreEqual(true, result2);
+            bool result3 = ProyectoFinalArqui.Program.accesoAddon("PREPAGO LIBRE");
+            Assert.AreEqual(false, result3);
         }
-        */
     }
 }
